62ffa4c28b629c6a65c59a553fff6dccc1a3f3a0
olatundeaiguru <olatunde.aiguru@gmail.com>
2025-11-18 09:51:45 +0100

    Remove database connection file the right way

--- database_connection.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
-db_url = "postgresql://postgres:TheGodsBlessedOne%401@localhost:5432/movie"
-engine = create_engine(db_url)
-
-session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
